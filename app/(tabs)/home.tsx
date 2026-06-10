53a2c6993d3adb222963462300af576104e4d1cc
Ecodexer <elhocine@ecodexia.com>
2026-06-10 15:47:14 +0100

    add home page with books card uisng scrollView & wip favorite or crud

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,14 +1,104 @@
 import Header from "@/components/common/Header";
-import { Text } from "react-native";
+import {
+  FlatList,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+const books = [
+  {
+    id: 1,
+    title: "Atomic Habits",
+    description: "Build good habits and break bad ones.",
+    image: "https://picsum.photos/200/300?random=1",
+  },
+  {
+    id: 2,
+    title: "The Alchemist",
+    description: "A journey of self-discovery.",
+    image: "https://picsum.photos/200/300?random=2",
+  },
+  {
+    id: 3,
+    title: "Deep Work",
+    description: "Rules for focused success.",
+    image: "https://picsum.photos/200/300?random=5",
+  },
+  {
+    id: 4,
+    title: "Rich Dad Poor Dad",
+    description: "What the rich teach their kids about money.",
+    image: "https://picsum.photos/200/300?random=5",
+  },
+  {
+    id: 5,
+    title: "Clean Code",
+    description: "A handbook of agile software craftsmanship.",
+    image: "https://picsum.photos/200/300?random=4",
+  },
+  {
+    id: 6,
+    title: "Rich Dad Poor Dad",
+    description: "What the rich teach their kids about money.",
+    image: "https://picsum.photos/200/300?random=5",
+  },
+];
 
 export default function HomeScreen() {
   return (
+    <SafeAreaView style={styles.container}>
+      <Header />
+      <ScrollView style={{flex: 1, padding: 8}}>
+        {
+          books.map((item, index) => {
+            return (
+              <View style={styles.card} key={index}>
+                <Image
+                  source={{ uri: item.image }}
+                  style={styles.image}
+                />
 
-    // fix Header inisde status bar using SafeAreaView
-    <SafeAreaView>
-      <Header/>
-      <Text>Hello Home</Text>
+                <Text style={styles.title}>{item.title}</Text>
+
+                <Text>
+                  {item.description}
+                </Text>
+              </View>
+            )
+          })
+        }
+      </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // remove grey div at the bottom of scroll
+    backgroundColor: "#fff"
+  },
+
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 12,
+    marginBottom: 15,
+    padding: 13,
+    elevation: 3, // Android shadow
+  },
+
+  image: {
+    width: '100%',
+    height: 140,
+  },
+
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+});
