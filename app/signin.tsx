5431698d05a11deb9935235b91263c6c39818696
Ecodexer <elhocine@ecodexia.com>
2026-06-16 17:22:44 +0100

    form login v1 beginner & wip

--- app/signin.tsx
+++ app/signin.tsx
@@ -1,7 +1,29 @@
 import { router } from "expo-router";
+import { useState } from "react";
 import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function signin() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleChangeEmail = (value: string) => {
+        setEmail(value)
+    }
+    const handleChangePassword = (value: string) => {
+        setPassword(value)
+    }
+
+    const handleSubmit = () => {
+        console.log(email)
+        console.log(password)
+
+        if(email === "admin@gmail.com" && password === "admin"){
+            router.push('/home')
+        }
+        // handle messages error
+    }
+
     return (
         <View style={styles.container}>
             <Image source={require('../assets/images/icon.png')} style={styles.image} />
@@ -12,6 +34,7 @@ export default function signin() {
                     <TextInput 
                         style={{borderWidth: 1, borderRadius: 8, padding: 10, borderColor: 'grey',color: 'grey'}}
                         placeholder="youremail@gmail.com"
+                        onChangeText={(value) => handleChangeEmail(value)}
                     />
                 </View>
                 <View style={{marginVertical: 5}}>
@@ -19,13 +42,15 @@ export default function signin() {
                     <TextInput 
                         style={{borderWidth: 1, borderRadius: 8, padding: 10, borderColor: 'grey', color: 'grey'}}
                         placeholder="your password"
+                        onChangeText={(value) => handleChangePassword(value)}
+
                     />
                 </View>
                 <TouchableOpacity 
                     style={{ backgroundColor: '#89CFF0', borderRadius: 15, padding: 10, marginTop: 10}}
-                    onPress={() => router.push('/home')}
+                    onPress={handleSubmit}
                 >
-                        <Text style={{textAlign: 'center', fontWeight: 600, fontSize: 15}}>Login</Text>
+                    <Text style={{textAlign: 'center', fontWeight: 600, fontSize: 15}}>Login</Text>
                 </TouchableOpacity>
             </View>
         </View>
